ee69b4123e8ffe71b8fcf85e1d54e4a3881797d2
Allancs <allancolombo@hotmail.com>
2017-08-17 01:16:59 -0300

    Asilo Arquivo INI

---
+++ Codigo/UPesquisaRel.pas
@@ -0,0 +1,42 @@
+unit UPesquisaRel;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, ExtCtrls, QuickRpt, QRCtrls;
+
+type
+  TPesquisaRel = class(TForm)
+    volu: TQuickRep;
+    ColumnHeaderBand1: TQRBand;
+    DetailBand1: TQRBand;
+    PageFooterBand1: TQRBand;
+    PageHeaderBand1: TQRBand;
+    TitleBand1: TQRBand;
+    QRDBText2: TQRDBText;
+    QRLabel3: TQRLabel;
+    QRLabel2: TQRLabel;
+    QRDBText1: TQRDBText;
+    QRLabel1: TQRLabel;
+    QRLabel4: TQRLabel;
+    QRDBText3: TQRDBText;
+    QRLabel5: TQRLabel;
+    QRLabel6: TQRLabel;
+    QRDBText4: TQRDBText;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  PesquisaRel: TPesquisaRel;
+
+implementation
+
+uses URelatorio;
+
+{$R *.dfm}
+
+end.
